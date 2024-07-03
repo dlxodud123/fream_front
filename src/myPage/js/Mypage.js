d4d80f7c7f53fc1b9ec7641038a13d8325992364
test_songhee <test@test.com>
2024-07-03 09:30:52 +0900

    -

--- src/myPage/js/Mypage.js
+++ src/myPage/js/Mypage.js
@@ -4,6 +4,7 @@ import '../css/Shortcut_grid.css';
 import '../css/User_membership.css';
 import '../css/Purchase.css';
 import '../css/Selling.css';
+import MypageList from './MypageList.js';
 import Header from '../../common/header';
 import Footer from '../../common/footer';
 import { Link, useNavigate } from 'react-router-dom';
@@ -23,48 +24,8 @@ return (
     <div className='snb_area'>
     <div className="row">
       <div className="col-sm-3">
-        <div className="mypage">
-          <h2>마이페이지</h2>
-        </div>
-        <div>
-          <h2 className="snb_title">쇼핑 정보</h2>
-            <ul className="menu">
-              <li className="list-item">
-                <Link to={"/my/buying"}>구매 내역</Link></li>
-              <li className="list-item">
-                <Link to={"/my/selling"}>판매 내역</Link></li>
-              <li className="list-item">
-                <Link to={"/my/inventory"}>보관 판매</Link></li>
-              <li className="list-item">
-                <Link to={"/my/saved"}>관심</Link></li>
-            </ul>
-        </div>
-        <div className="section">
-          <h2 className="snb_title">내 정보</h2>
-            <ul className="menu">
-              <li className="list-item">
-                <Link to={"/my/profile"}>로그인 정보</Link></li>
-              <li className="list-item">
-                <Link to={"/my/profile-edit"}>프로필 관리</Link></li>
-              <li className="list-item">
-                <Link to={"/my/seller-tier"}>판매자 등급</Link></li>
-              <li className="list-item">
-                <Link to={"/my/address"}>주소록</Link></li>
-              <li className="list-item">
-                <Link to={"/my/payment"}>결제 정보</Link></li>
-              <li className="list-item">
-                <Link to={"/my/account"}>판매 정산 계좌</Link></li>
-              <li className="list-item">
-                <Link to={"/my/receipt"}>현금영수증 정보</Link></li>
-              <li className="list-item">
-                <Link to={"/my/point"}>포인트</Link></li>
-              <li className="list-item">
-                <Link to={"/my/coupon"}>쿠폰</Link></li>
-            </ul>
-        </div>
+        <MypageList />
       </div>
-
-
 
 
       <div className="col-sm-9">
