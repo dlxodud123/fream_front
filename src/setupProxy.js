733b0121b4e46487e64e7fb147aefe1309701844
junhyeong9812 <pickjog@gmail.com>
2024-07-29 23:45:29 +0900

    배포준비

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,10 +4,11 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http:58.125.92.250:3001//", // 백엔드 서버 주소
+      // target: "http://localhost:3001",
+      target: "http://58.125.92.250:3001//", // 백엔드 서버 주소
       // target: "http://192.168.42.142:3001", // 백엔드 서버 주소
       changeOrigin: true,
-      cookieDomainRewrite: "localhost", // 프록시가 쿠키 도메인을 localhost로 재작성하도록 설정
+      cookieDomainRewrite: "www.pinjun.xyz", // 프록시가 쿠키 도메인을 localhost로 재작성하도록 설정
     })
   );
 };
